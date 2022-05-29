bfd279cdaaff077f9fd7c4338145f6f158f5d348
Hanseul Lee <70684334+HS0204@users.noreply.github.com>
2022-05-30 04:19:39 +0900

    로그인, 회원가입 기능 구현
    파이어베이스 활용

--- app/src/main/java/hs/project/chattingapp/LoginActivity.kt
+++ app/src/main/java/hs/project/chattingapp/LoginActivity.kt
@@ -4,7 +4,9 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
 
@@ -13,9 +15,13 @@ class LoginActivity : AppCompatActivity() {
     private lateinit var loginBtn: Button
     private lateinit var signUpBtn: Button
 
+    private lateinit var mAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        mAuth = FirebaseAuth.getInstance()
 
         edtEmail = findViewById(R.id.edt_email)
         edtPwd = findViewById(R.id.edt_pwd)
@@ -27,5 +33,23 @@ class LoginActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        loginBtn.setOnClickListener {
+            val email = edtEmail.text.toString()
+            val pwd = edtPwd.text.toString()
+            login(email, pwd)
+        }
+
+    }
+
+    private fun login(email: String, pwd: String){
+        mAuth.signInWithEmailAndPassword(email, pwd)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val intent = Intent(this@LoginActivity, MainActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this@LoginActivity, "로그인에 실패하였습니다", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
--- app/src/main/java/hs/project/chattingapp/SignUpActivity.kt
+++ app/src/main/java/hs/project/chattingapp/SignUpActivity.kt
@@ -1,11 +1,49 @@
 package hs.project.chattingapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class SignUpActivity : AppCompatActivity() {
+
+    private lateinit var edtName: EditText
+    private lateinit var edtEmail: EditText
+    private lateinit var edtPwd: EditText
+    private lateinit var signUpBtn: Button
+
+    private lateinit var mAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        mAuth = FirebaseAuth.getInstance()
+
+        edtName = findViewById(R.id.edt_name)
+        edtEmail = findViewById(R.id.edt_email)
+        edtPwd = findViewById(R.id.edt_pwd)
+        signUpBtn = findViewById(R.id.signUpBtn)
+
+        signUpBtn.setOnClickListener {
+            val email = edtEmail.text.toString()
+            val pwd = edtPwd.text.toString()
+            signUp(email, pwd)
+        }
+    }
+
+    private fun signUp(email: String, pwd: String){
+        mAuth.createUserWithEmailAndPassword(email, pwd)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val intent = Intent(this@SignUpActivity, MainActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this@SignUpActivity, "회원가입에 실패하였습니다", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
