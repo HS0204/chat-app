576a6c2e14dea3106f45192beecef9d4d8798e47
Hanseul Lee <70684334+HS0204@users.noreply.github.com>
2022-05-30 17:44:12 +0900

    user어댑터

--- app/src/main/java/hs/project/chattingapp/MainActivity.kt
+++ app/src/main/java/hs/project/chattingapp/MainActivity.kt
@@ -1,11 +1,21 @@
 package hs.project.chattingapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var userRecyclerView: RecyclerView
+    private lateinit var userList: ArrayList<User>
+    private lateinit var adapter: UserAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        userList = ArrayList()
+        adapter = UserAdapter(this, userList)
+
     }
 }
