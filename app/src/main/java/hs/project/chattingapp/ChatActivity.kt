7034394a33f83f2f5348598dca2b6e9584f6b10a
Hanseul Lee <70684334+HS0204@users.noreply.github.com>
2022-05-30 20:17:02 +0900

    파이어베이스 메시지 업로드

--- app/src/main/java/hs/project/chattingapp/ChatActivity.kt
+++ app/src/main/java/hs/project/chattingapp/ChatActivity.kt
@@ -5,25 +5,58 @@ import android.widget.EditText
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class ChatActivity : AppCompatActivity() {
 
-    private lateinit var messageRecyclerView: RecyclerView
+    private lateinit var chatRecyclerView: RecyclerView
     private lateinit var messageBox: EditText
     private lateinit var sendButton: ImageView
+
+    private lateinit var messageAdapter: MessageAdapter
+    private lateinit var messageList: ArrayList<Message>
+
+    private lateinit var mDbRef: DatabaseReference
+
+    var receiverRoom: String? = null
+    var senderRoom: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
 
         val name = intent.getStringExtra("name")
-        val uid = intent.getStringExtra("uid")
+        val receiverUid = intent.getStringExtra("uid")
+        val senderUid = FirebaseAuth.getInstance().currentUser?.uid
+
+        mDbRef = FirebaseDatabase.getInstance().getReference()
+
+        senderRoom = receiverUid + senderUid
+        receiverRoom = senderUid + receiverUid
 
         supportActionBar?.title = name
 
-        messageRecyclerView = findViewById(R.id.charRecyclerView)
+        chatRecyclerView = findViewById(R.id.chatRecyclerView)
         messageBox = findViewById(R.id.messageBox)
         sendButton = findViewById(R.id.sendButton)
+
+        // 어댑터가 뷰에 메시지 전달
+        messageList = ArrayList()
+        messageAdapter = MessageAdapter(this, messageList)
+
+        // DB로 메시지 전송
+        sendButton.setOnClickListener {
+            val message = messageBox.text.toString()
+            val messageObject = Message(message, senderUid)
+            // "firebase DB 내 chats 안 송수신자UID 안 message 안 값 저장"이 성공할 경우
+            mDbRef.child("chats").child(senderRoom!!).child("message").push()
+                .setValue(messageObject).addOnSuccessListener {
+                    mDbRef.child("chats").child(receiverRoom!!).child("message").push()
+                        .setValue(messageObject)
+            }
+        }
 
     }
 }
